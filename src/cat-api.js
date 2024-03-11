f7d75be33114a9e0a8bf0f7d1f251d0f6b5f4db7
olsen1229 <marylou.olsen7@gmail.com>
2024-03-11 20:36:19 +0800

    initial commit

---
+++ src/cat-api.js
@@ -0,0 +1,26 @@
+const BASE_URL = "https://api.thecatapi.com/v1";
+const API_KEY = "live_oLklPYUp3iTMxGKwQITghEyymS1FrXwISwwHpuunCk61lgGxaJV3V3xg9sLtiJ3s";
+
+
+export function fetchBreeds() {
+    return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then((res) => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        } else {
+            return res.json();
+        }
+    });
+
+}
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then((res) => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        } else {
+            return res.json();
+        }
+    });
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+
+const breedSelectEl = document.querySelector(".breed-select");
+const catInfoEl = document.querySelector(".cat-info");
+const loaderEl = document.querySelector(".loader");
+const errorEl = document.querySelector(".error");
+
+errorEl.classList.add("is-hidden");
+
+
+function chooseBreed() {
+    fetchBreeds().then((data) => {
+        loaderEl.classList.replace("loader", "is-hidden");
+        
+        let optionsMarkup = data.map(({ name, id }) => {
+            return `<option value=${id}>${name}</option>`;
+        });
+        breedSelectEl.insertAdjacentElement("beforeend", optionsMarkup);
+        breedSelectEl.classList.remove("is-hidden");
+    })
+    .catch(onError);
+}
+
+chooseBreed();
+
+breedSelectEl.addEventListener("change", (e) => {
+
+    loaderEl.classList.replace("is-hidden", "loader");
+
+  
+    catInfoEl.classList.add("is-hidden");
+
+
+    let breedId = e.target.value;
+
+    fetchCatByBreed(breedId).then((data) => {
+        const { url, breeds } = data[0];
+        const { name, description, temperament } = breeds[0];
+
+        catInfoEl.innerHTML = `
+            <img src='${url}' alt='{name}' width="400"/>
+            <div class='box'>
+                <h2>${name}</h2>
+                <p>${description}</p>
+                <p>${temperament}</p>
+            </div>
+
+        `;
+            catInfoEl.classList.remove("is-hidden");
+                   
+            loaderEl.classList.add("is-hidden");
+        })
+        .catch(onError);
+});
+    
+
+    function onError() {
+        errorEl.classList.remove("is-hidden");
+
+        breedSelectEl.classList.add("is-hidden");
+    }
+
